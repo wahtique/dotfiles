083702fcef2887e5072efed18c9090ede8f6b198
wahtique <williamvphan@yahoo.fr>
2022-06-09 22:14:16 +0200

    Update dot_SpaceVim.d/init.toml Update dot_zshrc Update run_once-install-deps.zsh Update run_once_install-patched-fonts.zsh

--- run_once_install-patched-fonts.zsh
+++ run_once_install-patched-fonts.zsh
@@ -1,13 +1,21 @@
 #!/usr/bin/env zsh
 
-if [[ $(fc-list | grep -i "Powerline" | wc -l) -eq 0 ]]; then
-    echo "Installing Powerline patched fonts..."
-    workspace="/tmp/powerline-install-space"
-    mkdir -p $workspace
-    cd $workspace
-    git clone git@github.com:powerline/fonts.git
-    cd fonts
-    ./install.sh
+if [[ $(fc-list | grep -i "Nerd Font" | wc -l) -eq 0 ]]; then
+    accepted_choices=('y' 'n')
+    choice=''
+    while [[ $accepted_choices[(I)$choice] -eq 0 ]]; do
+      choice=''
+      vared -p 'Install patched Nerd Fonts ? [y/n] : ' -c choice
+    done
+    if [[ $choice = 'y' ]]; then
+      echo "Installing Powerline patched fonts..."
+      workspace="/tmp/powerline-install-space"
+      mkdir -p $workspace
+      cd $workspace
+      git clone --depth 1 git@github.com:ryanoasis/nerd-fonts.git 
+      cd nerd-fonts
+      ./install.sh
+    fi
 else 
     echo "Powerline patched fonts already installed"
 fi
