b1b6110ed9b72f827d062055e32369156a17092a
wahtique <williamvphan@yahoo.fr>
2023-06-25 18:14:41 +0200

    Update .config/home-manager/home.nix

--- dot_config/home-manager/home.nix
+++ dot_config/home-manager/home.nix
@@ -18,18 +18,27 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
+    # shell
+    pkgs.zi
+    pkgs.oh-my-posh
+    pkgs.fzf
+    pkgs.fd
+    pkgs.bat
+    pkgs.exa
+    pkgs.navi
+    pkgs.zoxide
+    pkgs.cod
     pkgs.htop
-    pkgs.fortune
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
+    pkgs.neofetch
+    (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+    # dev
+    pkgs.git
+    # java
+    pkgs.jdk17
+    # scala
+    pkgs.coursier
+    pkgs.sbt
+    pkgs.scala-cli
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
