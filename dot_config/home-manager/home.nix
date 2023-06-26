4b1c5e6744d6b81b894c1f6b5958c03e1de41e2c
wahtique <williamvphan@yahoo.fr>
2023-06-26 13:29:16 +0200

    Update .config/home-manager/home.nix

--- dot_config/home-manager/home.nix
+++ dot_config/home-manager/home.nix
@@ -37,7 +37,7 @@
     pkgs.git
     (builtins.fetchGit {
         url = "https://github.com/nix-community/nix-direnv.git";
-        ref = "latest";
+        ref = "refs/tags/latest";
       }
     )
     # java
