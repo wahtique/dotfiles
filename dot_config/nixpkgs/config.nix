ef7c468a5abbd91a1c461d1ecb3fa999620baf9c
wahtique <williamvphan@yahoo.fr>
2024-02-04 18:32:23 +0100

    Update .config/home-manager/home.nix Update .config/nixpkgs/config.nix

--- dot_config/nixpkgs/config.nix
+++ dot_config/nixpkgs/config.nix
@@ -3,8 +3,5 @@
     nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
       inherit pkgs;
     };
-    rust-overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz") {
-      inherit pkgs;
-    };
   };
 }
