0a2c076625ee16f8895f38269d527d914a6ab858
wahtique <williamvphan@yahoo.fr>
2024-02-19 14:08:47 +0100

    banner => toilet

--- run_after_update-all.sh
+++ run_after_update-all.sh
@@ -1,32 +1,40 @@
 #!/usr/bin/env zsh
 
-banner NIX
+if command -v toilet >/dev/null 2>&1; then
+  toilet -F border UPDATING
+fi
 
-# update nix packages
-if command -v nix-channel >/dev/null 2>&1; then
+if command -v nix >/dev/null 2>&1; then
+  # possibly not avaialable on first run
+  if command -v banner >/dev/null 2>&1; then
+    toilet NIX
+  else
+    echo "############ NIX ############"  
+  fi
+  # update nix packages
   nix-channel --update
+  # cleanup old generations
+  nix-collect-garbage --delete-older-than 30d
+
 fi
 
 # build new derivation
 if command -v home-manager >/dev/null 2>&1; then
+  toilet HOME
   home-manager switch
 fi
 
-# cleanup old generations
-if command -v nix-collect-garbage >/dev/null 2>&1; then
-  nix-collect-garbage --delete-older-than 30d
-fi
-
 # init shell
-
+#
 source ~/.zshrc
 
 # update zi then update zsh plugins
 if command -v zi >/dev/null 2>&1; then
-  banner ZI
+  toilet ZI
   zi self-update -n
   zi update --all --reset -q -n
+  echo "\n"
 fi
 
-banner DONE
+toilet -F border DONE
 
