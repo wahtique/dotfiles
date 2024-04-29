52751aab08f05353f6b0707ce10f8296be5412bd
wahtique <williamvphan@yahoo.fr>
2024-04-30 00:25:06 +0200

    cleanup: rm all symlinks
    as they are handled by the plugins

--- dot_local/share/zinit/completions/symlink__enhancd_command_run.fish
+++
@@ -1,1 +0,0 @@
-/home/wahtique/.local/share/zinit/plugins/b4b4r07---enhancd/functions/_enhancd_command_run.fish
--- dot_local/share/zinit/completions/symlink__enhancd_source_mru.fish
+++
@@ -1,1 +0,0 @@
-/home/wahtique/.local/share/zinit/plugins/b4b4r07---enhancd/functions/_enhancd_source_mru.fish
