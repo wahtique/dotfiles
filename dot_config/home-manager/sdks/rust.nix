2a56a1b5f0abf4c5858c555c21156b0a2b47bf8d
wahtique <william.veal-phan@ik.me>
2024-05-16 10:22:30 +0200

    Add .cargo/config.toml Update .config/home-manager/home.nix Update .config/home-manager/sdks/rust.nix

--- dot_config/home-manager/sdks/rust.nix
+++ dot_config/home-manager/sdks/rust.nix
@@ -10,13 +10,19 @@
   ];
   home = {
     packages = with pkgs; [
-      (fenix.complete.withComponents [
+      (fenix.stable.withComponents [
         "cargo"
         "clippy"
         "rust-src"
         "rustc"
         "rustfmt"
       ])
+      cargo-deny # generate list of licenses 
+      cargo-nextest # next gen test runner
+      cargo-tarpaulin # code coverage
+      cargo-release # release management
+      sqlx-cli # sqlx cli
+      mold # faster linker
       rust-analyzer
     ];
     sessionPath = [ "$HOME/.cargo/bin" ];
