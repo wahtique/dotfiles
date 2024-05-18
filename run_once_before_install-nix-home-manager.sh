124fc223e8a3b8e960d805f445d1d5bfcc882793
William Veal Phan <williamvphan@yahoo.fr>
2024-05-18 20:07:20 +0200

    Update run_once_before_install-nix-home-manager.sh

--- run_once_before_install-nix-home-manager.sh
+++ run_once_before_install-nix-home-manager.sh
@@ -5,6 +5,7 @@ if ! command -v nix >/dev/null 2>&1; then
 fi
 
 if ! command -v home-manager >/dev/null 2>&1; then
+  nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
   nix-channel --update
   nix-shell '<home-manager>' -A install
   home-manager switch
