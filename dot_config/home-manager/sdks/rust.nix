4b022740caec23b391b8a46cb3f37a4352499d04
wahtique <wvealphan@netwo.com>
2024-05-22 16:06:21 +0200

    fix: intellij + rust
    rm rust overlay

--- dot_config/home-manager/sdks/rust.nix
+++ dot_config/home-manager/sdks/rust.nix
@@ -10,18 +10,21 @@
   ];
   home = {
     packages = with pkgs; [
-      (fenix.stable.withComponents [
-        "cargo"
-        "clippy"
-        "rust-src"
-        "rustc"
-        "rustfmt"
-      ])
-      cargo-deny # generate list of licenses 
-      cargo-nextest # next gen test runner
-      cargo-tarpaulin # code coverage
-      cargo-release # release management
-      sqlx-cli # sqlx cli
+      # tmp use rustup manually until "cannot use rustfmt: rustup not found" in intellij is fixed
+      # see https://youtrack.jetbrains.com/issue/RUST-14013/Cant-use-rustfmt-without-rustup
+      #  
+      # (fenix.stable.withComponents [
+      #   "cargo"
+      #   "clippy"
+      #   "rust-src"
+      #   "rustc"
+      #   "rustfmt"
+      # ])
+      # cargo-deny # generate list of licenses 
+      # cargo-nextest # next gen test runner
+      # cargo-tarpaulin # code coverage
+      # cargo-release # release management
+      # sqlx-cli # sqlx cli
       mold-wrapped # faster linker
       rust-analyzer
     ];
