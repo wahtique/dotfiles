f2bdfc6a35d48cfc3f43810eda2ef10199f2d93d
wahtique <wvealphan@netwo.com>
2024-05-27 16:42:18 +0200

    put back rust sdk

--- dot_config/home-manager/sdks/rust.nix
+++ dot_config/home-manager/sdks/rust.nix
@@ -13,18 +13,18 @@
       # tmp use rustup manually until "cannot use rustfmt: rustup not found" in intellij is fixed
       # see https://youtrack.jetbrains.com/issue/RUST-14013/Cant-use-rustfmt-without-rustup
       #  
-      # (fenix.stable.withComponents [
-      #   "cargo"
-      #   "clippy"
-      #   "rust-src"
-      #   "rustc"
-      #   "rustfmt"
-      # ])
-      # cargo-deny # generate list of licenses 
-      # cargo-nextest # next gen test runner
-      # cargo-tarpaulin # code coverage
-      # cargo-release # release management
-      # sqlx-cli # sqlx cli
+      (fenix.stable.withComponents [
+        "cargo"
+        "clippy"
+        "rust-src"
+        "rustc"
+        "rustfmt"
+      ])
+      cargo-deny # generate list of licenses 
+      cargo-nextest # next gen test runner
+      cargo-tarpaulin # code coverage
+      cargo-release # release management
+      sqlx-cli # sqlx cli
       mold-wrapped # faster linker
       rust-analyzer
     ];
