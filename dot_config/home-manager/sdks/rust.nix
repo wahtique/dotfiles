96aac19bc47cfb6266bfec577f3eb480623eb509
wahtique <william.veal-phan@ik.me>
2024-10-25 14:58:30 +0200

    feat(rust): add cargo-mutants

--- dot_config/home-manager/sdks/rust.nix
+++ dot_config/home-manager/sdks/rust.nix
@@ -24,6 +24,7 @@
       cargo-nextest # next gen test runner
       cargo-tarpaulin # code coverage
       cargo-release # release management
+      cargo-mutants # mutation testing
       sqlx-cli # sqlx cli
       mold-wrapped # faster linker
       rust-analyzer
