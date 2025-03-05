48df64a4d0c2888710da99ec84c48ca478ea0bdf
wahtique <wvealphan@netwo.com>
2025-03-05 14:25:52 +0100

    chore(node): disable corepack auto pining

--- dot_config/home-manager/sdks/node.nix
+++ dot_config/home-manager/sdks/node.nix
@@ -1,8 +1,13 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [
-    nodejs
-    nodePackages.npm
-    nodePackages.ts-node
-    corepack_latest
-  ];
+  home = {
+    packages = with pkgs; [
+      nodejs
+      nodePackages.npm
+      nodePackages.ts-node
+      corepack_latest
+    ];
+    sessionVariables = {
+      COREPACK_ENABLE_AUTO_PIN = "0";
+    };
+  };
 }
