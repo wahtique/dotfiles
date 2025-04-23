cae4de67d4af4a1ebfc4c93f2bb03825bc0aa178
wahtique <wvealphan@netwo.com>
2025-04-23 16:47:33 +0200

    chore(zsh): put everythin in initContent

--- dot_config/home-manager/sdks/scala.nix
+++ dot_config/home-manager/sdks/scala.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   home.packages = with pkgs; [ 
     scala-cli
     metals # lsp
@@ -15,7 +15,7 @@
         sbtc = "sbt --client";
         scala = "scala-cli";
       };
-      initExtraBeforeCompInit = ''
+      initContent = lib.mkBefore ''
         fpath+=(
           "$HOME/.local/share/scalacli/completions/zsh"
         )
