8d412c57c2ff0ab65cad178895b75e208a4c5e2e
wahtique <wvealphan@netwo.com>
2025-09-05 17:09:52 +0200

    stylix: avoid conflicts on backup files

--- dot_config/home-manager/stylix.nix
+++ dot_config/home-manager/stylix.nix
@@ -1,8 +1,18 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 let
   stylix = builtins.fetchTarball "https://github.com/nix-community/stylix/archive/master.tar.gz";
 in
 {
+  home.activation = {
+    # gtk backup files fomr previous generations can randomly be left behind
+    # which makes the build fail
+    # clean those up before writing the new config
+    cleanGtkBackups = lib.hm.dag.entryBefore [ "checkLinkTargets" ] ''
+      rm -f $HOME/.gtkrc-2.0.backup
+      rm -f $HOME/.config/gtk-4.0/*.backup
+      rm -f $HOME/.config/gtk-3.0/*.backup
+    '';
+  };
   imports = [ (import stylix).homeModules.stylix ];
   stylix = {
     enable = true;
