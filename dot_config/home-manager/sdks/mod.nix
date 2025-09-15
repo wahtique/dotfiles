7559615da363d31651f9a9c8cd4154274f6ab90b
wahtique <wvealphan@netwo.com>
2025-09-15 09:40:45 +0200

    refactor: sdks/sdks.nix -> sdk/mod.nix

---
+++ dot_config/home-manager/sdks/mod.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    lua
+    # ansible
+    terraform
+    terraform-ls
+    lldb # debugger for native code
+    gopls
+  ];
+
+  programs = {
+    go.enable = true;
+  };
+}
