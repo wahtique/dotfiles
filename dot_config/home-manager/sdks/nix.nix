d91cd0ff2707fef52ed5bb329675a73869d52cc4
wahtique <wvealphan@netwo.com>
2026-01-12 14:02:29 +0100

    chore: nixfmt

--- dot_config/home-manager/sdks/nix.nix
+++ dot_config/home-manager/sdks/nix.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     nil # nix lsp
-    nixfmt-rfc-style # opinionated formatter
+    nixfmt # opinionated formatter
     cachix # derivation caching
     nh # nix helper
     nix-output-monitor # nom
