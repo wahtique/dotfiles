1afda4e48ed71d1e889aa9defd5c3e218cd0bc3a
wahtique <wvealphan@netwo.com>
2026-02-13 10:20:26 +0100

    patch: vscode 1.109.2 by override while unstable is not released

--- dot_config/home-manager/gui/code.nix
+++ dot_config/home-manager/gui/code.nix
@@ -3,7 +3,12 @@
   programs = {
     vscode = {
       enable = true;
-      package = pkgs.vscode;
+      # override package until unstable catches up
+      package = pkgs.vscode.overrideDerivation (oldAttrs: {
+        name = "VSCode_1.109.2_x86_64-linux.tar.gz";
+        url = "https://update.code.visualstudio.com/1.109.2/x86_64-linux/stable";
+        hash = "sha256-ST5i8gvNtAaBbmcpcg9GJipr8e5d0A0qbdG1P9QViek=";
+      });
     };
   };
   stylix.targets.vscode.enable = false;
