964b3a5b2c6af636a1b4bf5f3e380efd357d0478
wahtique <wvealphan@netwo.com>
2026-02-13 14:31:37 +0100

    code 1.109.3

--- dot_config/home-manager/gui/code.nix
+++ dot_config/home-manager/gui/code.nix
@@ -3,29 +3,11 @@
   programs = {
     vscode = {
       enable = true;
-      # override package until unstable catches up
-      # package = pkgs.vscode.overrideDerivation (
-      #   oldAttrs:
-      #   let
-      #     # hash = "sha256-ST5i8gvNtAaBbmcpcg9GJipr8e5d0A0qbdG1P9QViek=";
-      #     version = "1.109.2";
-      #     plat = "linux-x64";
-      #     archive_fmt = "tar.gz";
-      #   in
-      #   {
-      #     src = builtins.fetchurl {
-      #       name = "VSCode_${version}_${plat}.${archive_fmt}";
-      #       url = "https://update.code.visualstudio.com/${version}/${plat}/stable";
-      #       # inherit hash;
-      #     };
-
-      #   }
-      # );
       package = pkgs.vscode.overrideAttrs (oldAttrs: rec {
-        hash = "sha256-ST5i8gvNtAaBbmcpcg9GJipr8e5d0A0qbdG1P9QViek=";
+        hash = "sha256-fTpXK5cbhyZqG4cW28K8XRd/QU9zerydWnLzFuhA9NE=";
         plat = "linux-x64";
         archive_fmt = "tar.gz";
-        version = "1.109.2";
+        version = "1.109.3";
         src = pkgs.fetchurl {
           name = "VSCode_${version}_${plat}.${archive_fmt}";
           url = "https://update.code.visualstudio.com/${version}/${plat}/stable";
