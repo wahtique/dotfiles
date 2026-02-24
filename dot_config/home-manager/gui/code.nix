91269f6e8201b2b6134d91d4dc7dd7ed648f2fb7
wahtique <wvealphan@netwo.com>
2026-02-24 15:43:31 +0100

    code: explicitly set package because unstable is still lagging behind

--- dot_config/home-manager/gui/code.nix
+++ dot_config/home-manager/gui/code.nix
@@ -4,10 +4,10 @@
     vscode = {
       enable = true;
       package = pkgs.vscode.overrideAttrs (oldAttrs: rec {
-        hash = "sha256-fTpXK5cbhyZqG4cW28K8XRd/QU9zerydWnLzFuhA9NE=";
+        hash = "sha256-cBYBA8DdpQ9lGjfuJ6ZammSmk4c0zxoLzUnYNhK9sac=";
         plat = "linux-x64";
         archive_fmt = "tar.gz";
-        version = "1.109.3";
+        version = "1.109.5";
         src = pkgs.fetchurl {
           name = "VSCode_${version}_${plat}.${archive_fmt}";
           url = "https://update.code.visualstudio.com/${version}/${plat}/stable";
