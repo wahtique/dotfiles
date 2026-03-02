74164f36a950c4b264fe47b01f59150be917e53a
wahtique <wvealphan@netwo.com>
2026-03-02 10:36:20 +0100

    code: rm package override

--- dot_config/home-manager/gui/code.nix
+++ dot_config/home-manager/gui/code.nix
@@ -1,19 +1,19 @@
-{ pkgs, ... }:
+{ ... }:
 {
   programs = {
     vscode = {
       enable = true;
-      package = pkgs.vscode.overrideAttrs (oldAttrs: rec {
-        hash = "sha256-cBYBA8DdpQ9lGjfuJ6ZammSmk4c0zxoLzUnYNhK9sac=";
-        plat = "linux-x64";
-        archive_fmt = "tar.gz";
-        version = "1.109.5";
-        src = pkgs.fetchurl {
-          name = "VSCode_${version}_${plat}.${archive_fmt}";
-          url = "https://update.code.visualstudio.com/${version}/${plat}/stable";
-          inherit hash;
-        };
-      });
+      # package = pkgs.vscode.overrideAttrs (oldAttrs: rec {
+      #   hash = "sha256-cBYBA8DdpQ9lGjfuJ6ZammSmk4c0zxoLzUnYNhK9sac=";
+      #   plat = "linux-x64";
+      #   archive_fmt = "tar.gz";
+      #   version = "1.109.5";
+      #   src = pkgs.fetchurl {
+      #     name = "VSCode_${version}_${plat}.${archive_fmt}";
+      #     url = "https://update.code.visualstudio.com/${version}/${plat}/stable";
+      #     inherit hash;
+      #   };
+      # });
     };
   };
   stylix.targets.vscode.enable = false;
