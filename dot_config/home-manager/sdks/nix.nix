dee0fb043455166c25a2fc3017ae73775b2efa42
wahtique <wvealphan@netwo.com>
2026-04-22 15:51:28 +0200

    nix: add some utilities

--- dot_config/home-manager/sdks/nix.nix
+++ dot_config/home-manager/sdks/nix.nix
@@ -5,6 +5,11 @@
     cachix # derivation caching
     nh # nix helper
     nix-output-monitor # nom
+    nix-init # generate nix pkg from url
+    nix-tree # browse dep graph of derivation
+    nix-sweep # cleanup old generations and gc roots
+    nix-update # update version / hash of nix package
+    nurl # generate nix fetcher from url
   ];
   programs = {
     nix-your-shell = {
